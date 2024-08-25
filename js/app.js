7c7a7b39459db3e39fcc57c5a76adfebc5ff7f3f
dilshod <dilshodx516@gmail.com>
2024-08-25 07:03:17 +0500

    js

---
+++ js/app.js
@@ -0,0 +1,81 @@
+// 1-masala
+// {
+//     let a = prompt("Kvadratning tomonini kiriting:");
+//     let P = 4 * a;
+//     console.log(`Kvadratning c ${P}cm`);
+// }
+
+// // 2-masala
+// {
+//     let b = prompt("Kvadratning tomonini kiriting:");
+//     let S = b * b;
+//     console.log(`Kvadratning yuzasi ${S}`);
+// }
+
+// 3 - masala
+// {
+//     let a = +prompt("To'rtburchak a  tomonini kiriting:");
+//     let b = +prompt("To'rtburchak b tomonini kiriting:");
+//     console.log("To'rtburchak yuzasi " + a * b, "To'rtburchak peruimetri " + 2 * (a + b));
+// }
+
+// 4 - masala
+// {
+//     let d = +prompt("Aylana diametrini kiriting");
+
+//     p = 3.14;
+//     l = d * p;
+//     console.log("Aylana uzunligi" + l);
+// }
+
+// 5- masala
+// {
+//     let a = +prompt("To'rtburchak a  tomonini kiriting:");
+//     let b = +prompt("To'rtburchak b tomonini kiriting:");
+//     console.log("To'rtburchak yuzasi " + a * b, "To'rtburchak peruimetri " + 2 * (a + b));
+// }
+
+// 6-masala
+// {
+//     let a = +prompt("Paralelepeped a  tomonini kiriting:");
+//     let b = +prompt("Paralelepeped b  tomonini kiriting:");
+//     let c = +prompt("Paralelepeped c  tomonini kiriting:");
+//     v = a * b * c;
+//     s = 2 * (a * b + b * c + a * c);
+//     console.log(v);
+//     console.log(s);
+// }
+
+// 7-masala
+// {
+//     let r = +prompt("Aylana radiusini kiriting");
+//     p = 3.14;
+//     l = 2 * r * p;
+//     s = p * (r * r);
+
+//     console.log("Aylananing uzunligi" + " " + l, "Aylananing yuzasi" + " " + s);
+// }
+// 8 - masala
+// {
+//     let a = +prompt("Birinchi son")
+//     let b = +prompt("Ikkinchi son")
+//     const urta = (`${(a + b) / 2}`)
+//     console.log(urta);
+// }
+
+
+// 9 - masala
+// {
+//     let a = +prompt("Birinchi son")
+//     let b = +prompt("Ikkinchi son")
+//     const urta = (`${(a * b) / 2}`)
+//     console.log(urta);
+// }
+
+// 10 - masala
+
+// {
+//     let a = +prompt("Birinchi sonni kiriting");
+//     let b = +prompt("Ikkinchi sonni kiriting");
+//     console.log("Yig'indisi " + a + b, "Ko'paytmasi " + a * b, "Kvadrati " + a * a, "Kvadrati " + b * b);
+// }
